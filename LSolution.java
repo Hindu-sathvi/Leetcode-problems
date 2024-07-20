2e25ee1800d3fb3a3014df034bc08fdd0bd3bbf9
Hindu-sathvi <hindujareddy426@gmail.com>
2024-07-20 17:11:21 +0530

    Add files via upload

---
+++ LSolution.java
@@ -0,0 +1,41 @@
+/*https://leetcode.com/problems/longest-palindromic-substring/ */
+/* longest-palindromic-substring*/
+/*code */
+public class Lsolution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        if (n == 0) return "";
+        
+        boolean[][] dp = new boolean[n][n];
+        int maxLen = 1;
+        int start = 0;
+     
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                maxLen = 2;
+                start = i;
+            }
+        }
+        
+
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    if (len > maxLen) {
+                        maxLen = len;
+                        start = i;
+                    }
+                }
+            }
+        }
+        
+        return s.substring(start, start + maxLen);
+    }
+}
