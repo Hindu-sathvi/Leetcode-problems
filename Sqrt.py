9b9d43078586867417e5166f29d1b02b78f1e0a5
Hinduja Reddy Kora <hindujareddy426@gmail.com>
2024-08-13 21:39:14 +0530

    Add files via upload

---
+++ Sqrt.py
@@ -0,0 +1,21 @@
+#https://leetcode.com/problems/sqrtx/description/?envType=study-plan-v2&envId=top-interview-150
+#code:
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 2:
+            return x
+        left, right = 1, x // 2
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return right
