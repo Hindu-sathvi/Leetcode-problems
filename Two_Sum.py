4272a49a48dd9c651d08408fe644828e2f398071
Hinduja Reddy Kora <hindujareddy426@gmail.com>
2024-11-09 12:57:41 +0530

    Update Two_Sum.py

--- Two_Sum.py
+++ Two_Sum.py
@@ -14,3 +14,21 @@ class Solution(object):
                if(nums[i]+nums[j]==target):
                       return [i, j] 
         return []
+#Implementing using Hashmap
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        #Implementing using hashmap
+        prevmap={}
+        for i,n in enumerate(nums):
+            diff=target-n
+            if diff in prevmap:
+                return[prevmap[diff],i]
+            prevmap[n]=i
+        return
+
+        
