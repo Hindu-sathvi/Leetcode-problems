2c9dbfffe04bd28d7cac82986043cd5941b41ada
Hinduja Reddy Kora <hindujareddy426@gmail.com>
2024-11-10 12:41:03 +0530

    Update sametree.py

--- sametree.py
+++ sametree.py
@@ -22,3 +22,47 @@ class Solution(object):
         traverse(p,result1)
         traverse(q,result2)
         return result1 == result2 
+# another solution of sametree
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: Optional[TreeNode]
+        :type q: Optional[TreeNode]
+        :rtype: bool
+        """
+        stack = [(p, q)]
+        
+        while stack:
+            node1, node2 = stack.pop()
+            if not node1 and not node2:
+                continue
+            if not node1 or not node2 or node1.val != node2.val:
+                return False
+            stack.append((node1.left, node2.left))
+            stack.append((node1.right, node2.right))
+        return True
+#Another solution of SameTree
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: Optional[TreeNode]
+        :type q: Optional[TreeNode]
+        :rtype: bool
+        """
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        return (p.val == q.val) and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
