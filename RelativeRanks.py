f3848c642068e970026b8243741e12ac9cf7129f
Hinduja Reddy Kora <hindujareddy426@gmail.com>
2024-11-14 23:27:20 +0530

    Create RelativeRanks.py

---
+++ RelativeRanks.py
@@ -0,0 +1,20 @@
+#https://leetcode.com/problems/relative-ranks/description/?envType=problem-list-v2&envId=heap-priority-queue
+#Code:
+class Solution(object):
+    def findRelativeRanks(self, score):
+        """
+        :type score: List[int]
+        :rtype: List[str]
+        """
+        sorted_scores = sorted(score, reverse=True)
+        rank_map = {}
+        for i, s in enumerate(sorted_scores):
+            if i == 0:
+                rank_map[s] = "Gold Medal"
+            elif i == 1:
+                rank_map[s] = "Silver Medal"
+            elif i == 2:
+                rank_map[s] = "Bronze Medal"
+            else:
+                rank_map[s] = str(i + 1)
+        return [rank_map[s] for s in score]
