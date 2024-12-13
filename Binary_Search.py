c07ee8940a4c52af0c2989642e916689b7ecd732
Hinduja Reddy Kora <hindujareddy426@gmail.com>
2024-12-13 19:53:05 +0530

    Create Binary_Search.py

---
+++ Binary_Search.py
@@ -0,0 +1,21 @@
+#https://leetcode.com/problems/binary-search/?envType=problem-list-v2&envId=binary-search&difficulty=EASY
+#Code:
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid  
+            elif nums[mid] < target:
+                left = mid + 1  
+            else:
+                right = mid - 1  
+    
+        return -1 
+        
