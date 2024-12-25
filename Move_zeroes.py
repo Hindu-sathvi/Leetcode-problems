cb9862facec7a6c2f9516d0bb096acd7684a3cc9
Hinduja Reddy Kora <hindujareddy426@gmail.com>
2024-12-25 15:47:43 +0530

    Create Move_zeroes.py

---
+++ Move_zeroes.py
@@ -0,0 +1,13 @@
+#https://leetcode.com/problems/move-zeroes/?envType=problem-list-v2&envId=two-pointers
+#Code:
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        left = 0
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1 
