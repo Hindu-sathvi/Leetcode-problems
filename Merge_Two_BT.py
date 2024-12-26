a89327a5babad22da7815d5eac8577b4b4e869ce
Hinduja Reddy Kora <hindujareddy426@gmail.com>
2024-12-26 22:06:12 +0530

    Create Merge_Two_BT.py

---
+++ Merge_Two_BT.py
@@ -0,0 +1,27 @@
+#https://leetcode.com/problems/merge-two-binary-trees/?envType=problem-list-v2&envId=depth-first-search
+#Code:
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def mergeTrees(self, root1, root2):
+        """
+        :type root1: Optional[TreeNode]
+        :type root2: Optional[TreeNode]
+        :rtype: Optional[TreeNode]
+        """
+        if not root1 and not root2:
+            return None
+        elif not root1:
+            return root2
+        elif not root2:
+            return root1
+        else:
+            merge_node = TreeNode(root1.val + root2.val)
+            merge_node.left = self.mergeTrees(root1.left, root2.left)
+            merge_node.right = self.mergeTrees(root1.right, root2.right)
+            return merge_node
+        
