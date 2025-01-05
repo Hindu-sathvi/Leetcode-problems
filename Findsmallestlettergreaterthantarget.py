e3075142e5d31f5aff4a9c4decdaba049995ffd2
Hinduja Reddy Kora <hindujareddy426@gmail.com>
2025-01-05 20:53:57 +0530

    Create Findsmallestlettergreaterthantarget.py

---
+++ Findsmallestlettergreaterthantarget.py
@@ -0,0 +1,19 @@
+#https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/?envType=problem-list-v2&envId=binary-search
+#Code:
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+        left, right = 0, len(letters) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if letters[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return letters[left % len(letters)]
+        
