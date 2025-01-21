1643d251e981d9eca1882c7721ad7e93112a52ba
Hinduja Reddy Kora <hindujareddy426@gmail.com>
2025-01-21 22:36:04 +0530

    Create Minimum_Cost_climbing_stairs.py

---
+++ Minimum_Cost_climbing_stairs.py
@@ -0,0 +1,16 @@
+#https://leetcode.com/problems/min-cost-climbing-stairs/?envType=problem-list-v2&envId=dynamic-programming
+#Code:
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        n = len(cost)
+        dp = [0] * (n + 1)  
+
+        for i in range(2, n + 1):
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
+        return dp[n]
+
+        
